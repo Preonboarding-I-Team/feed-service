0144ed997e7ac204f8015b6723be435e7a9a7af4
qushe8r <qushe8r@gmail.com>
2023-10-25 14:14:48 +0900

    Feat: 회원 가입 요청 컨트롤러 구현 (#2)
    이 커밋에서는 회원 가입 컨트롤러를 구현하였습니다.
    
    단위 테스트:
    - account 값 null 체크
    - email 이메일 null 체크, email pattern 체크
    - password 체크
      - 비밀번호는 최소 10자 이상이어야 합니다.
      - 통상적으로 자주 사용되는 비밀번호는 사용할 수 없습니다.
      - 숫자, 문자, 특수문자 중 2가지 이상을 포함해야 합니다.
      - 3회 이상 연속되는 문자 사용이 불가합니다.

---
+++ src/main/java/com/wanted/preonboarding/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.wanted.preonboarding.user.service.impl;
+
+import com.wanted.preonboarding.user.dto.UserPost;
+import com.wanted.preonboarding.user.dto.UserResponse;
+import com.wanted.preonboarding.user.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public UserResponse save(UserPost post) {
+        return null;
+    }
+}
