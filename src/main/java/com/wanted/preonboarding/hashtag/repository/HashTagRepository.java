4ae5fe607f410f50e5c9976d3739bf0cba4d3704
lala9663 <lala96632040@gmail.com>
2023-10-27 17:22:28 +0900

    feat: #10 FeedHashTag, Hashtag repo 생성

--- src/main/java/com/wanted/preonboarding/hashtag/repository/HashTagRepository.java
+++ src/main/java/com/wanted/preonboarding/hashtag/repository/HashTagRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
+public interface HashTagRepository extends JpaRepository<Hashtag, Long> {
     Hashtag findByName(String name);
 }
