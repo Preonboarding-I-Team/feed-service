da0fe680479cd71c43c2024f6cc1bbcde4efe858
lala9663 <lala96632040@gmail.com>
2023-10-27 17:10:29 +0900

    feat: FeedRepository 추가 #10-feat-게시글api

---
+++ src/main/java/com/wanted/preonboarding/feed/repositroy/FeedRepository.java
@@ -0,0 +1,10 @@
+package com.wanted.preonboarding.feed.repositroy;
+
+import com.wanted.preonboarding.feed.entity.Feed;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FeedRepository extends JpaRepository<Feed, Long> {
+
+}
