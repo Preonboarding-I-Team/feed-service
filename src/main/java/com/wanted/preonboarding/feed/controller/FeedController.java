57ebb5a4f89f80b55b148b96e65628e831cda19f
lala9663 <lala96632040@gmail.com>
2023-10-30 01:43:08 +0900

    feat: #10 피드 리스트조회 controller 구현

--- src/main/java/com/wanted/preonboarding/feed/controller/FeedController.java
+++ src/main/java/com/wanted/preonboarding/feed/controller/FeedController.java
@@ -1,14 +1,15 @@
 package com.wanted.preonboarding.feed.controller;
 
 import com.wanted.preonboarding.feed.dto.CreateFeedDto;
+import com.wanted.preonboarding.feed.dto.FeedDto;
+import com.wanted.preonboarding.feed.dto.UpdateFeedDto;
+import com.wanted.preonboarding.feed.entity.Feed;
 import com.wanted.preonboarding.feed.service.FeedService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/feed")
@@ -20,6 +21,12 @@ public class FeedController {
     public ResponseEntity<String> createBoard(@RequestBody @Valid CreateFeedDto createFeedDto) {
         feedService.createFeed(createFeedDto);
         return ResponseEntity.ok("게시글이 등록되었습니다");
+    }
+
+    @GetMapping("/{feedId}")
+    public ResponseEntity<FeedDto> getFeedDetails(@PathVariable Long feedId) {
+        FeedDto feedDto = feedService.feedDetailById(feedId);
+        return ResponseEntity.ok(feedDto);
     }
 }
 
