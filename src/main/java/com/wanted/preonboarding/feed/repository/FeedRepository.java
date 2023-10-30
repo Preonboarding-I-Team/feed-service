c1ec471c768857b4c0bb34b6d47d737eb011bfc9
lala9663 <lala96632040@gmail.com>
2023-10-30 15:43:38 +0900

    feat: #10 타입으로 검색(FACEBOOK, INSTAGRAM 등등) - repository

--- src/main/java/com/wanted/preonboarding/feed/repository/FeedRepository.java
+++ src/main/java/com/wanted/preonboarding/feed/repository/FeedRepository.java
@@ -1,6 +1,7 @@
 package com.wanted.preonboarding.feed.repository;
 
 import com.wanted.preonboarding.feed.entity.Feed;
+import com.wanted.preonboarding.feed.entity.FeedType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,5 +11,6 @@ import org.springframework.stereotype.Repository;
 public interface FeedRepository extends JpaRepository<Feed, Long>, FeedRepositoryCustom {
     Page<Feed> findByTitleContaining(String keyword, Pageable pageable);
     Page<Feed> findByContentContaining(String keyword, Pageable pageable);
+    Page<Feed> findByType(FeedType type, Pageable pageable);
 
 }
