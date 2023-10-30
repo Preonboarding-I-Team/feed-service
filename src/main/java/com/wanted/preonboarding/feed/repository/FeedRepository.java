936c37578d2e9ad75829f9964d5fd5f413b3e517
lala9663 <lala96632040@gmail.com>
2023-10-30 14:18:51 +0900

    feat: #10 피드 검색 - repository 추가

--- src/main/java/com/wanted/preonboarding/feed/repository/FeedRepository.java
+++ src/main/java/com/wanted/preonboarding/feed/repository/FeedRepository.java
@@ -1,9 +1,14 @@
 package com.wanted.preonboarding.feed.repository;
 
 import com.wanted.preonboarding.feed.entity.Feed;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FeedRepository extends JpaRepository<Feed, Long>, FeedRepositoryCustom {
+    Page<Feed> findByTitleContaining(String keyword, Pageable pageable);
+    Page<Feed> findByContentContaining(String keyword, Pageable pageable);
+
 }
