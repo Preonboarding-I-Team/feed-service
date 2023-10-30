5257fe39d6dc739fdc8cba83bc4fc4be2bc05330
lala9663 <lala96632040@gmail.com>
2023-10-30 15:43:52 +0900

    feat: #10 타입으로 검색(FACEBOOK, INSTAGRAM 등등) - controller

--- src/main/java/com/wanted/preonboarding/feed/controller/FeedController.java
+++ src/main/java/com/wanted/preonboarding/feed/controller/FeedController.java
@@ -2,7 +2,9 @@ package com.wanted.preonboarding.feed.controller;
 
 import com.wanted.preonboarding.feed.dto.CreateFeedDto;
 import com.wanted.preonboarding.feed.dto.FeedResponseDto;
+import com.wanted.preonboarding.feed.dto.UpdateFeedDto;
 import com.wanted.preonboarding.feed.entity.Feed;
+import com.wanted.preonboarding.feed.entity.FeedType;
 import com.wanted.preonboarding.feed.service.FeedService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -24,10 +26,15 @@ public class FeedController {
         feedService.createFeed(createFeedDto);
         return ResponseEntity.ok("게시글이 등록되었습니다");
     }
+    @PutMapping("/{feedId}")
+    public ResponseEntity<Feed> updateFeed(@PathVariable Long feedId, @RequestBody @Valid UpdateFeedDto updateFeedDto) {
+        Feed updatedFeed = feedService.updateFeed(feedId, updateFeedDto);
+        return new ResponseEntity<>(updatedFeed, HttpStatus.OK);
+    }
     @GetMapping("/{feedId}")
     public ResponseEntity<FeedResponseDto> getFeedDetails(@PathVariable Long feedId) {
-        FeedResponseDto feedDto = feedService.feedDetailById(feedId);
-        return ResponseEntity.ok(feedDto);
+        FeedResponseDto feedResponseDto = feedService.feedDetailById(feedId);
+        return ResponseEntity.ok(feedResponseDto);
     }
 
     @GetMapping("/list")
@@ -53,12 +60,17 @@ public class FeedController {
         }
         return new ResponseEntity<>(feedPage, HttpStatus.OK);
     }
-
-
-    @PutMapping("/{feedId}")
-    public ResponseEntity<Feed> updateFeed(@PathVariable Long feedId, @RequestBody @Valid UpdateFeedDto updateFeedDto) {
-        Feed updatedFeed = feedService.updateFeed(feedId, updateFeedDto);
-        return new ResponseEntity<>(updatedFeed, HttpStatus.OK);
+    @GetMapping("/searchByType")
+    public ResponseEntity<Page<FeedResponseDto>> searchByType(
+            @RequestParam(name = "type", required = false, defaultValue = "") FeedType type,
+            Pageable pageable
+    ) {
+        if (type == null) {
+            return new ResponseEntity<>(feedService.feedList(pageable), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(feedService.getFeedsByType(type, pageable), HttpStatus.OK);
+        }
     }
+
 }
 
