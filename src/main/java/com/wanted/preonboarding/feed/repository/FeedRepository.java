b8b60039a7be32f1772883b0bb8490c911bd8ecc
soonhankwon <soonable@gmail.com>
2023-10-30 10:11:03 +0900

    feat: FeedRepositoryCustom 인터페이스 상속(#4)

--- src/main/java/com/wanted/preonboarding/feed/repository/FeedRepository.java
+++ src/main/java/com/wanted/preonboarding/feed/repository/FeedRepository.java
@@ -1,10 +1,9 @@
-package com.wanted.preonboarding.feed.repositroy;
+package com.wanted.preonboarding.feed.repository;
 
 import com.wanted.preonboarding.feed.entity.Feed;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FeedRepository extends JpaRepository<Feed, Long> {
-
+public interface FeedRepository extends JpaRepository<Feed, Long>, FeedRepositoryCustom {
 }
