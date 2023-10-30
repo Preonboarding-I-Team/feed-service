11ca6ea6cacd90561dd137589268faf7e5dab0e0
lala9663 <lala96632040@gmail.com>
2023-10-30 12:53:48 +0900

    feat: #10 피드 제목,내용으로 검색 구현 - repository

--- src/main/java/com/wanted/preonboarding/feed/repositroy/FeedRepository.java
+++ src/main/java/com/wanted/preonboarding/feed/repositroy/FeedRepository.java
@@ -1,10 +1,13 @@
 package com.wanted.preonboarding.feed.repositroy;
 
 import com.wanted.preonboarding.feed.entity.Feed;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FeedRepository extends JpaRepository<Feed, Long> {
-
+    Page<Feed> findByTitleContaining(String keyword, Pageable pageable);
+    Page<Feed> findByContentContaining(String keyword, Pageable pageable);
 }
