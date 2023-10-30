59697bffff556612c7dce10b26d0aeb9e38f65c6
soonhankwon <soonable@gmail.com>
2023-10-30 11:49:17 +0900

    feat: name 컬럼 인덱싱-where절에 해시태그 이름 일치 쿼리 사용(#4)

--- src/main/java/com/wanted/preonboarding/hashtag/entity/Hashtag.java
+++ src/main/java/com/wanted/preonboarding/hashtag/entity/Hashtag.java
@@ -10,17 +10,21 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table(name = "hashtag")
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
+@Table(name = "hashtag", indexes = {
+        @Index(name = "idx_name_idx", columnList = "name")})
 public class Hashtag {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "hashtag_id")
     private Long hashTagId;
+
     private String name;
+
     @OneToMany(mappedBy = "hashtag")
     private Set<FeedHashTag> feedHashTag = new HashSet<>();
 
