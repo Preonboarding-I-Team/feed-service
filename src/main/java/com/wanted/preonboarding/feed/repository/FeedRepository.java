cfb0ba8d9b9396f1ed4d39bb2c4ab2a3bfb13520
lala9663 <lala96632040@gmail.com>
2023-10-30 17:05:35 +0900

    feat: #10 해시태그로 검색 - repository

--- src/main/java/com/wanted/preonboarding/feed/repository/FeedRepository.java
+++ src/main/java/com/wanted/preonboarding/feed/repository/FeedRepository.java
@@ -5,6 +5,8 @@ import com.wanted.preonboarding.feed.entity.FeedType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,7 +14,8 @@ public interface FeedRepository extends JpaRepository<Feed, Long>, FeedRepositor
     Page<Feed> findByTitleContaining(String keyword, Pageable pageable);
     Page<Feed> findByContentContaining(String keyword, Pageable pageable);
     Page<Feed> findByTitleContainingOrContentContaining(String title, String content, Pageable pageable);
-
     Page<Feed> findByType(FeedType type, Pageable pageable);
+    @Query("SELECT f FROM Feed f JOIN f.feedHashTag fh JOIN fh.hashtag h WHERE h.name = :hashtag")
+    Page<Feed> findByHashtagName(@Param("hashtag") String hashtag, Pageable pageable);
 
 }
