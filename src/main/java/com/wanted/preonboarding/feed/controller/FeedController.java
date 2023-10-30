cfb27db5f02b4c5ce482d2d7522aac6fe9dc9e6f
lala9663 <lala96632040@gmail.com>
2023-10-30 12:55:13 +0900

    feat: #10 피드 제목,내용으로 검색 구현 - controller

--- src/main/java/com/wanted/preonboarding/feed/controller/FeedController.java
+++ src/main/java/com/wanted/preonboarding/feed/controller/FeedController.java
@@ -7,6 +7,9 @@ import com.wanted.preonboarding.feed.entity.Feed;
 import com.wanted.preonboarding.feed.service.FeedService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,10 +26,28 @@ public class FeedController {
         return ResponseEntity.ok("게시글이 등록되었습니다");
     }
 
-    @GetMapping("/{feedId}")
-    public ResponseEntity<FeedDto> getFeedDetails(@PathVariable Long feedId) {
-        FeedDto feedDto = feedService.feedDetailById(feedId);
-        return ResponseEntity.ok(feedDto);
+    @GetMapping("/list")
+    public ResponseEntity<Page<FeedDto>> feedList(@PageableDefault(page = 0, size = 10) Pageable pageable) {
+        Page<FeedDto> feedPage = feedService.feedList(pageable);
+        return new ResponseEntity<>(feedPage, HttpStatus.OK);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<Page<FeedDto>> searchFeeds(
+            @RequestParam String search_by,
+            @RequestParam String keyword,
+            Pageable pageable
+    ) {
+        Page<FeedDto> feedPage;
+
+        if ("title".equalsIgnoreCase(search_by)) {
+            feedPage = feedService.searchByTitle(keyword, pageable);
+        } else if ("content".equalsIgnoreCase(search_by)) {
+            feedPage = feedService.searchByContent(keyword, pageable);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(feedPage, HttpStatus.OK);
     }
 }
 
